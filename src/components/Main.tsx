2ea6b8feeaf434f1a0b9e00e366195c30225647f
Gregg Cline <ggcline82@gmail.com>
2025-12-19 17:01:59 -0800

    Insatlled Shadcn

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,23 +8,24 @@ export default function Main() {
  };
 
  return (
-  <main class="bg-sky-500">
-   <div class="bg-gray-600 flex flex-row gap-8">
-    <div class="w-64 bg-amber-300 px-3 py-2">
-     <h2>Account Balance $4,000.00</h2>
+  <main class="bg-white">
+   <div class="bg-white py-8 px-6 flex flex-row gap-8">
+    <div class="w-64 bg-amber-300 px-3 py-2 rounded-md flex flex-col content-center ">
+     <h2 class="text-black text-center">Account Balance </h2>
+     <h3 class="text-black text-center">$4,000.00</h3>
     </div>
-    <div class="w-64 bg-amber-800 px-3 py-2">
+    <div class="w-64 bg-purple-500 rounded-md  px-3 py-2">
      <h2>Balance After Bills $1,945.00</h2>
     </div>
-    <div class="w-64 bg-amber-800 px-3 py-2">
+    <div class="w-64 bg-green-700 rounded-md  px-3 py-2">
      <h2>Total Income $5,000.00</h2>
     </div>
     <div>
-     <h2 class="w-64 bg-amber-600 px-3 py-2">Total Bills</h2>
+     <h2 class="w-64 bg-blue-600 rounded-md px-3 py-2">Total Bills</h2>
     </div>
    </div>
    <div>
-    <div class="text-2xl mt-64 flex justify-center">Transactions</div>
+    <div class="text-2xl mt-64 text-black flex justify-center">Transactions</div>
     <li>
      <input
       class="w-50 border text-black border-gray-300 px-3 py-2 rounded-lg shadow-sm outline-none focus:border-indigo-500 focus:ring-indigo-500 focus:ring-1"
@@ -32,8 +33,22 @@ export default function Main() {
       value={inputValue()}
       onInput={handleInput}></input>
     </li>
-    <li>Item 2</li>
-    <li>item 3</li>
+    <li>
+     {' '}
+     <input
+      class="w-50 border text-black border-gray-300 px-3 py-2 rounded-lg shadow-sm outline-none focus:border-indigo-500 focus:ring-indigo-500 focus:ring-1"
+      type="text"
+      value={inputValue()}
+      onInput={handleInput}></input>
+    </li>
+    <li>
+     {' '}
+     <input
+      class="w-50 border text-black border-gray-300 px-3 py-2 rounded-lg shadow-sm outline-none focus:border-indigo-500 focus:ring-indigo-500 focus:ring-1"
+      type="text"
+      value={inputValue()}
+      onInput={handleInput}></input>
+    </li>
    </div>
   </main>
  );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { A } from '@solidjs/router';
 
 export default function Home() {
  return (
-  <main class="text-center mx-auto text-gray-700 p-4">
+  <main class="text-center mx-auto text-black p-4">
    <h1 class="text-4xl">Body</h1>
   </main>
  );
